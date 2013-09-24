d96bcded65dd6a95b95f9c19be2ca0c77f54684f
James Chevalier <jchevalier@gmail.com>
2013-09-24 13:22:20 -0400

    add spec_helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+ENV['RACK_ENV'] = 'test'
+require_relative '../app'
+require 'minitest/autorun'
+require 'rack/test'
+
+class MiniTest::Spec
+  include Rack::Test::Methods
+
+  def app
+    Rack::Builder.parse_file(File.dirname(__FILE__) + '/../config.ru').first
+  end
+end
